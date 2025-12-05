13f6f6cf6267a16e7764fd42443a3fc95cd497b9
Kllmplz <alvarogarciaramos98@gmail.com>
2025-12-05 14:22:05 +0100

    Archivos para GitHub pages y docs automaticos

---
+++ principal.py
@@ -0,0 +1,8 @@
+def suma(a, b):
+    """
+    Calculará la suma de dos números
+    :param a: Primer número.
+    :param b: Segundo número.
+    :return: La suma de a y de b
+    """
+    return a + b
